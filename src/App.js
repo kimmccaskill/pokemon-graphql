04b1f608af99b09247fc06912ac3e6472a2c1dda
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-30 20:24:17 -0600

    Fix plural form naming convention

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { PokemonsContainer } from './containers/PokemonsContainer';
+import { PokemonContainer } from './containers/PokemonContainer';
 
 export function App() {
-    const client = new ApolloClient({
-        uri: 'https://graphql-pokemon.now.sh'
-    });
-    
-    return (
-        <ApolloProvider client={client}>
-            <main>
-                <PokemonsContainer />
-            </main>
-        </ApolloProvider>
-    )
+  const client = new ApolloClient({
+    uri: 'https://graphql-pokemon.now.sh'
+  });
+  
+  return (
+    <ApolloProvider client={client}>
+      <main>
+        <PokemonContainer />
+      </main>
+    </ApolloProvider>
+  )
 }
